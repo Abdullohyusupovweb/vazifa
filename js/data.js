9bcf764ba3f4356238869ffc3fef005246c2aa6c
Abdulloh <befbcebfwncery@gmail.com>
2025-04-09 17:38:16 +0500

    vazifam

---
+++ js/data.js
@@ -0,0 +1,13 @@
+export default [
+
+  {
+    title: "intro-section-with-dropdown-navigation",
+    github: "https://github.com/Abdullohyusupovweb/8-dars",
+    vercel: "https://8-dars-ruddy-xi.vercel.app/",
+  },
+
+];
+
+
+
+
