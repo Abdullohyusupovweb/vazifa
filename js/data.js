d5c278bee106b4ee61904f00708793c410e0ac46
Abdulloh <befbcebfwncery@gmail.com>
2025-04-11 19:39:26 +0500

    .

--- js/data.js
+++ js/data.js
@@ -1,5 +1,9 @@
 export default [
-
+  {
+    title: "soatlar boyicha sayt",
+    github: "https://github.com/Abdullohyusupovweb/9-dars.vazifa",
+    vercel: "https://9-dars-vazifa-omega.vercel.app/",
+  },
   {
     title: "intro-section-with-dropdown-navigation",
     github: "https://github.com/Abdullohyusupovweb/8-dars",
